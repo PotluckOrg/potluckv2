8255054d1980db3c8215906203544aa593381162
Natalie Ung <ung.natalie@gmail.com>
2018-02-01 13:25:55 -0500

    market component

--- client/components/ItemCard.js
+++ client/components/ItemCard.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
 const ItemCard = (props) => {
+    const user = props.user
     const item = props.item
-    const inPantry = props.history.params.match === '/pantry'
-    const buttonText = inPantry ? <i className="fa fa-plus" aria-hidden="true" /> : <i className="fa fa-times" aria-hidden="true" />
+    // const inPantry = props.history.params.match === '/pantry'
+    // const inMarket = props.history.params.match === '/market'
+    // const buttonText = inPantry || inMarket ? <i className="fa fa-plus" aria-hidden="true" /> : <i className="fa fa-times" aria-hidden="true" />
     return (
         <div className="card w-100">
             <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
+                <h5 className="card-title">{item.itemName}</h5>
                 <p className="card-text">{item.description}</p>
-                <a href="#" className="btn">{buttonText}</a>
+                <button className="btn" onClick={event => handleAddToBasket(event, item.id, user.id)}>HI</button>
             </div>
         </div>
     )
 }
+
+
+// mapDispatchToProps = (dispatch, ownProps) => {
+//     return {
+//         handleAddToBasket: (event, itemId, userId) => {
+//             return () => {
+//                 dispatch(addToBasket(itemId, userId))
+//             }
+//         }
+//     }
+// }
 
 export default ItemCard
