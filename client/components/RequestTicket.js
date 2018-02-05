867538763b82877ff94dbf24978cd80f376ef04d
lawyatt88 <lemonawyatt@gmail.com>
2018-02-05 17:36:15 -0500

    updated request ticket wiring for updating contract

--- client/components/RequestTicket.js
+++ client/components/RequestTicket.js
@@ -33,35 +33,7 @@ const RequestTicket = (props) => {
         itemsRequested.push(item)
     })
 
-    // currently wired up to recieve only one item fro mthe contract
-
-    // itemsRequested.push(items.find(item => {
-    //     return item.id === request.contractAssociation.itemId
-    // }))
-
     let card;
-
-    // if (request.status === 'Pending') {
-    //     card = (
-    //         <div className="card-body">
-    //             <h5 className="card-title">{request.userId}</h5>
-    //             <h5 className="card-title">User you're trading with: {item.name}</h5>
-    //             <h5 className="card-title">Item you'll receive: {item.name}</h5>
-    //             <h5 className="card-title">Item you're trading: {item.name}</h5>
-    //             <p>Item description: {item.description}</p>
-    //             <button>Confirm that you've received your food!</button>
-    //         </div>
-    //     )
-    // }
-
-    // if (request.status === 'Completed') {
-    //     card = (
-    //         <div className="card-body">
-    //             <h5 className="card-title">{request.userId}</h5>
-    //             <h5 className="card-title">You completed a trade with:  {item.name}</h5>
-    //         </div>
-    //     )
-    // }
 
     return (
         <div className="request-ticket">
@@ -78,7 +50,8 @@ const RequestTicket = (props) => {
                 {(offer ? (
                     <div>
                         <h3>Offer</h3>
-                        <div class="btn" onClick={() => updateContractHandler([{name: '1 eggplant', id: 1, description: 'lol'}], )}>Want an Eggplant?</div>
+                        <div className="btn" onClick={() => updateContractHandler([{name: '1 eggplant', id: 1, description: 'lol', userId: currentUser.id}], request, sender, currentUser )}>
+                        Want an Eggplant?</div>
                     
                     </div>
                 ) : null)}
