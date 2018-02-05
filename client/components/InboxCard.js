83bedb24f9c1055f30de710668a769f4adc9ee41
Natalie Ung <ung.natalie@gmail.com>
2018-02-05 12:11:22 -0500

    FEATURE TradeInbox filtering by status

--- client/components/InboxCard.js
+++ client/components/InboxCard.js
@@ -4,15 +4,12 @@ import { fetchAllItems } from '../store'
 
 
 const InboxCard = (props) => {
-    console.log('PROPS IN INBOXCARD', props)
     const { request } = props
     const item = props.items.find(item => {
         return item.id === request.contractAssociation.itemId
     })
 
     let card;
-    console.log('REQUEST', request)
-    card = <h3>i'm a card!!</h3>
 
     if (request.status === 'Pending') {
         card = 
@@ -31,20 +28,9 @@ const InboxCard = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{request.userId}</h5>
                 <h5 className="card-title">You completed a trade with:  {item.name}</h5>
-                <button>Confirm that you've received your food and receive Potluck Points!</button>
             </div>
     }
 
-    // if (request.status.completed) {
-    //     card =
-    //     <div className="card-body">
-    //             <h5 className="card-title">{request.userId}</h5>
-    //             <h5 className="card-title">User you traded with: {item.name}</h5>
-    //             <h5 className="card-title">Item you received: {item.name}</h5>
-    //             <h5 className="card-title">Item you traded: {item.name}</h5>
-    //             <button>Confirm that you've received your food!</button>
-    //         </div>
-    // }
 
     return (
         <a href={`/${request.id}`} className="card-link">
